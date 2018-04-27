069fc71e6275555d768c5a7d9c7e7eeae83ff551
Yiftach Karkason <ykarkason@gmail.com>
2018-04-27 22:32:52 +0300

    Removed non-portable tests

--- tests/pack_test.cpp
+++ tests/pack_test.cpp
@@ -11,10 +11,6 @@ TEST_CASE("pack sanity", "[cppystruct::pack]")
 {
     // Packing floats or doubles isn't constexpr, due to no constexpr reinterpret_cast...
 
-    REQUIRE_STATIC(pystruct::pack(PY_STRING("ci"), '*', 0x12131415) == "*\x00\x00\x00\x15\x14\x13\x12"sv);
-    REQUIRE(pystruct::pack(PY_STRING("bfi"), true, 0.5f, 1) == "\x01\x00\x00\x00\x00\x00\x00?\x01\x00\x00\x00"sv);
-    REQUIRE(pystruct::pack(PY_STRING("bdi"), true, 0.5, -1) == "\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xe0?\xff\xff\xff\xff"sv);
-
     REQUIRE_STATIC(pystruct::pack(PY_STRING(">ci"), '*', 0x12131415) == "*\x12\x13\x14\x15"sv);
     REQUIRE(pystruct::pack(PY_STRING(">bfi"), true, 0.5f, 1) == "\x01?\x00\x00\x00\x00\x00\x00\x01"sv);
     REQUIRE(pystruct::pack(PY_STRING(">bdi"), true, 0.5, -1) == "\x01?\xe0\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff"sv);
@@ -36,7 +32,7 @@ TEST_CASE("pack unsigned ints", "[cppystruct::pack]")
 
 TEST_CASE("pack signed ints", "[cppystruct::pack]")
 {
-    REQUIRE_STATIC(pystruct::pack(PY_STRING("bhilq"),  -127, -32767, -2147483647, -2147483647, -9223372036854775807) == "\x81\x00\x01\x80\x01\x00\x00\x80\x01\x00\x00\x80\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x80"sv);
+    REQUIRE_STATIC(pystruct::pack(PY_STRING("=bhilq"),  -127, -32767, -2147483647, -2147483647, -9223372036854775807) == "\x81\x00\x01\x80\x01\x00\x00\x80\x01\x00\x00\x80\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x80"sv);
     REQUIRE_STATIC(pystruct::pack(PY_STRING(">bhilq"), -127, -32767, -2147483647, -2147483647, -9223372036854775807) == "\x81\x80\x01\x80\x00\x00\x01\x80\x00\x00\x01\x80\x00\x00\x00\x00\x00\x00\x01"sv);
     REQUIRE_STATIC(pystruct::pack(PY_STRING("!bhilq"), -127, -32767, -2147483647, -2147483647, -9223372036854775807) == "\x81\x80\x01\x80\x00\x00\x01\x80\x00\x00\x01\x80\x00\x00\x00\x00\x00\x00\x01"sv);
     REQUIRE_STATIC(pystruct::pack(PY_STRING("<bhilq"), -127, -32767, -2147483647, -2147483647, -9223372036854775807) == "\x81\x01\x80\x01\x00\x00\x80\x01\x00\x00\x80\x01\x00\x00\x00\x00\x00\x00\x80"sv);
@@ -49,8 +45,8 @@ TEST_CASE("pack signed ints", "[cppystruct::pack]")
 
 TEST_CASE("pack string", "[cppystruct::pack]")
 {
-    REQUIRE_STATIC(pystruct::pack(PY_STRING("5s"), "12345") == "12345"sv);
-    REQUIRE_STATIC(pystruct::pack(PY_STRING("h5s"), 126, "12345") == "\x7e\x00""12345"sv);
+    REQUIRE_STATIC(pystruct::pack(PY_STRING("<5s"), "12345") == "12345"sv);
+    REQUIRE_STATIC(pystruct::pack(PY_STRING("<h5s"), 126, "12345") == "\x7e\x00""12345"sv);
     REQUIRE_STATIC(pystruct::pack(PY_STRING(">h5s"), 126, "12345") == "\x00\x7e""12345"sv);
 
     REQUIRE_STATIC(pystruct::pack(PY_STRING("3s"), "12345") == "123"sv);
